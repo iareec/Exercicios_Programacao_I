fa242f1d4f9352384593c9bca466e004a926dc42
iareec <14r33c@gmail.com>
2021-03-11 19:01:19 +0000

    Jogo da Carta
    Exercício resolvido.

---
+++ JogoDaCarta/Assets/Scripts/GeradorDeCartas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeCartas : MonoBehaviour
+{
+    string[] naipe = new string[] {"Paus", "Ouros", "Copas", "Espadas"};
+    string[] valor = new string[] {"o Ás", "o 2", "o 3", "o 4", "o 5", "o 6", "o 7", "o 8", "o 9", "o 10", "o Valete", "a Dama", "o Rei"};
+    void Start()
+    {
+        Iniciar();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GerarCarta();
+        }
+    }
+    void Iniciar() 
+    {
+        Debug.Log("Pressione na barra de espaço para gerar uma nova carta.");
+    }
+    void GerarCarta() 
+    {
+        string naipe_sorteado = naipe[Random.Range(0, naipe.Length)];
+        string valor_sorteado = valor[Random.Range(0, valor.Length)];
+        Debug.Log("Saiu " + valor_sorteado + " de " + naipe_sorteado + "!");
+        Iniciar();
+    }
+}
