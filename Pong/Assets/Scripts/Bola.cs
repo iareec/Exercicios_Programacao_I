74769b2f071406153b2a00da5a0d88789e06a276
iareec <14r33c@gmail.com>
2021-03-22 15:34:33 +0000

    Pong

---
+++ Pong/Assets/Scripts/Bola.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    [SerializeField] float velocidade = 5f;
+    bool lancamento = false;
+    void Update()
+    {
+        float cronometro = Time.time;
+        if (cronometro > 2.0 && lancamento == false)
+        {
+            lancamento = true;
+            lancarBola();
+        }
+    }
+    void lancarBola()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitCircle; // Lança para um lado aleatório (-1 a 1, -1 a 1)
+    }
+}
---
+++ Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float velocidade = 2f;
+    [SerializeField] KeyCode setaCima = KeyCode.UpArrow;
+    [SerializeField] KeyCode setaBaixo = KeyCode.DownArrow;
+
+    void Update()
+    {
+        if (Input.GetKey(setaCima)) // Seta para cima, sobe
+        {
+            transform.position += velocidade * Vector3.up * Time.deltaTime;
+        } if (Input.GetKey(setaBaixo)) // Seta para baixo, desce
+        {
+            transform.position += velocidade * Vector3.down * Time.deltaTime;
+        }
+        float alturaCamera = Camera.main.orthographicSize;
+        Vector3 posicaoAux = transform.position;
+        posicaoAux.y = Mathf.Clamp(posicaoAux.y, -alturaCamera + 0.5f, alturaCamera - 0.5f);
+        transform.position = posicaoAux;
+    }
+}
